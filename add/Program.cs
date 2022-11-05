7be1419e871499f3a757c0ddc2be499c6822a3bf
viktor <moiseev0506@mail.ru>
2022-11-05 21:39:40 +0300

    version 1

---
+++ add/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = new int[10];
+            Console.WriteLine($"Минимальное число: {For(Generation(first))}");
+            
+            Console.ReadKey();
+        }
+        static int[] Generation(int[] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]=random.Next(-50,50);
+            }
+            foreach (var item in arr)            
+            {
+                Console.WriteLine(item+"\t");
+            }
+            return arr;
+        }
+        static int For(int[] arr)
+        {
+            int first = arr[0];
+            int second = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (first > arr[i])
+                {
+                    first = arr[i];
+                    second = first;
+                }
+            }
+            return second;
+        }
+    }
+}
